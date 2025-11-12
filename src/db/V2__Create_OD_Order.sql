71992f1583e7cc1ddae8c6d5a107785dd6b8832d
FavianaT <favianat802@gmail.com>
2025-11-12 13:44:47 -0500

    _

--- src/db/V2__Create_OD_Order.sql
+++ src/db/V2__Create_OD_Order.sql
@@ -1,8 +1,6 @@
 CREATE TABLE OD_Order (
     Order_ID int NOT NULL PRIMARY KEY,
     Order_Quantity int NOT NULL,
-    Order_Date_TIME TIMESTAMP NOT NULL,
-    Item_Name VARCHAR(20) NOT NULL,
     Donut_ID int NOT NULL,
     FOREIGN KEY (Donut_ID) REFERENCES OD_Donut(Donut_ID)
 );
