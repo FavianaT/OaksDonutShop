8c5240afa13383d1eb596e49674d5e710590dc92
FavianaT <favianat802@gmail.com>
2025-11-15 16:58:38 -0500

    Update V2__Create_OD_Order.sql

--- src/db/V2__Create_OD_Order.sql
+++ src/db/V2__Create_OD_Order.sql
@@ -1,6 +1,5 @@
 CREATE TABLE OD_Order (
     Order_ID int NOT NULL PRIMARY KEY,
-    Order_Quantity int NOT NULL,
-    Donut_ID int NOT NULL,
-    FOREIGN KEY (Donut_ID) REFERENCES OD_Donut(Donut_ID)
+    Order_Price VARCHAR(20) NOT NULL,
+    Donut_Name VARCHAR(20) NOT NULL
 );
